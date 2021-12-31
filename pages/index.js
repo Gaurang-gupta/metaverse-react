83ecbf6eaafd01d92c2e9b95905a1a58ba588b65
Gaurang Gupta <g12172001@gmail.com>
2021-12-31 17:40:39 +0530

    Login and Logout functionality using moralis

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Login from '../components/Login'
+import { useMoralis } from 'react-moralis';
+
+export default function Home() {
+  const { user, isAuthenticated, logout } = useMoralis();
+  console.log(isAuthenticated)
+  if (!isAuthenticated) return <Login />
+  return (
+    <div className="h-screen">
+      <Head>
+        <title>Metaverse react</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Welcome to the app</h1>
+      <button onClick={logout}>Logout</button>
+    </div>
+  )
+}
